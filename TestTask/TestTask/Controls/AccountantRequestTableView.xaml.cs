703b57b5167a3ade06a029207c2d95038279638d
JuliaZorina <yulia.a.zzo@gmail.com>
2026-05-31 22:33:46 +0400

    Создано окно просмотра заявок бухгалтера

---
+++ TestTask/TestTask/Controls/AccountantRequestTableView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TestTask.DB;
+using TestTask.Services;
+
+namespace TestTask.Controls
+{
+  /// <summary>
+  /// Логика взаимодействия для AccountantRequestTableView.xaml
+  /// </summary>
+  public partial class AccountantRequestTableView : Window
+  {
+    public AccountantRequestTableView()
+    {
+      InitializeComponent(); var db = new ApplicationDbContext();
+      var requestService = new RequestService(db);
+      var requests = requestService.GetAllRequests();
+
+      if (requests != null)
+      {
+        this.RequestTable.ReuestsGrid.ItemsSource = requests;
+      }
+    }
+
+    private void Cancel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
