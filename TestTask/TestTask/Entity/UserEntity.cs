c9f9786c4c2b9fb09a53325e3e7d3654822e5c1a
JuliaZorina <yulia.a.zzo@gmail.com>
2026-05-31 14:24:18 +0400

    Доработаны сущности, настроен DbContext и sqlite

--- TestTask/TestTask/Entity/UserEntity.cs
+++ TestTask/TestTask/Entity/UserEntity.cs
@@ -5,5 +5,7 @@
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Role { get; set; }
+    public ICollection<Request> Requests { get; set; } = new List<Request>();
+
   }
 }
