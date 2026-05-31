dbbf53963623e1fc26fb9fe944c737d6e8c86572
JuliaZorina <yulia.a.zzo@gmail.com>
2026-05-31 16:45:49 +0400

    Созданы перечисления

---
+++ TestTask/TestTask/Enums/RequestStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace TestTask.Enums
+{
+  public enum RequestStatus
+  {
+    [Description("Создана")]
+    Created,
+
+    [Description("В работе")]
+    InProgress,
+
+    [Description("Завершена")]
+    Completed,
+
+    [Description("Отклонена")]
+    Rejected
+  }
+}
---
+++ TestTask/TestTask/Enums/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TestTask.Enums
+{
+  public enum UserRole
+  {
+    [Description("Сотрудник")]
+    Worker,
+    [Description("Бухгалтер")]
+    Accountant 
+  }
+}
