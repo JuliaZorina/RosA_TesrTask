71e3c94084e710646063282a63381e0c61ddf239
JuliaZorina <yulia.a.zzo@gmail.com>
2026-05-31 13:55:38 +0400

    Созданы модели для бд

---
+++ TestTask/TestTask/Entity/Request.cs
@@ -0,0 +1,13 @@
+﻿namespace TestTask.Entity
+{
+  public class Request
+  {
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string RequestTypeName { get; set; }
+    public int NumberOfRequests { get; set; }
+    public string Reason { get; set; }
+    public string Status { get; set; }
+  }
+}
