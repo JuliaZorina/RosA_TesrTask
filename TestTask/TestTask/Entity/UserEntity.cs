441043e11a20df5f64616f1f6dd5a2684f02111c
JuliaZorina <yulia.a.zzo@gmail.com>
2026-05-31 16:51:18 +0400

    В сущностях string заменено на enum

--- TestTask/TestTask/Entity/UserEntity.cs
+++ TestTask/TestTask/Entity/UserEntity.cs
@@ -1,10 +1,12 @@
-﻿namespace TestTask.Entity
+﻿using TestTask.Enums;
+
+namespace TestTask.Entity
 {
   public class UserEntity
   {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Role { get; set; }
+    public UserRole Role { get; set; }
     public ICollection<Request> Requests { get; set; } = new List<Request>();
 
   }
